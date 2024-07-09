a3f57bcb12dabab6594c38d40f901c8e6944d4af
shreyanmitra <shreyan.m.mitra+testing@gmail.com>
2024-07-09 03:16:36 -0700

    Update __init__.py

--- llm-wrapper/__init__.py
+++ llm-wrapper/__init__.py
@@ -2,9 +2,9 @@
 from pkg_resources import get_distribution, DistributionNotFound
 
 try:
-    dist = get_distribution("llm-wrapper")
+    dist = get_distribution("CandyLLM")
 except DistributionNotFound:
-    __version__ = "Please install llm-wrapper with setup.py"
+    __version__ = "Please install CandyLLM with setup.py"
 else:
     __version__ = dist.version
 
