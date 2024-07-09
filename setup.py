f77394ce3d703c516855415b29345ef0b0e04a91
shreyanmitra <shreyan.m.mitra+testing@gmail.com>
2024-07-09 03:13:27 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_namespace_packages
 
-setup(name='llm-wrapper',
+setup(name='CandyLLM',
       version='0.0.1', 
-      description='llm-wrapper: Unified framework for HuggingFace and OpenAI Text-generation Models',
+      description='CandyLLM: Unified framework for HuggingFace and OpenAI Text-generation Models',
       long_description=open("README.md", "r", encoding="utf-8").read(),
       long_description_content_type="text/markdown",
       keywords="llms transformers mistral llama falcon gpt-4 alpaca",
